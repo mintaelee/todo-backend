5155396f49b74f2b594a8eadb8a4ee2351961977
Mintae Lee <lee.mintae@codeimmersives.com>
2019-04-24 16:04:02 -0400

    finished all objectives

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 window.onload = init;
 
 function init() {
-    document.querySelector('#get').addEventListener('click', getTodos);
+    setDefault();
+
     document.querySelector('#post').addEventListener('click', postTodo);
-    document.querySelector('#put').addEventListener('click', updateThirdTodo);
+    document.querySelector('#delete').addEventListener('click', deleteCompleted);
 }
 
-function getTodos(event) {
-    event.preventDefault();
-
+function setDefault(){
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://localhost:3000/todos');
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -16,11 +15,23 @@ function getTodos(event) {
     xhr.send();
 }
 
+// function getTodos(event) {
+//     event.preventDefault();
+
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'http://localhost:3000/todos');
+//     xhr.setRequestHeader('Content-Type', 'application/json');
+//     xhr.onload = handleData;
+//     xhr.send();
+// }
+
 function postTodo(event) {
     event.preventDefault();
 
+    const userInput = document.querySelector(".user-input").value;
+
     const newTodo = {
-        text: "Profit",
+        text: userInput,
         completed: false
     };
     
@@ -33,20 +44,87 @@ function postTodo(event) {
     xhr.send(jsonnedTodo);
 }
 
-function updateThirdTodo(event) {
+function markComplete(element) {
+    event.preventDefault();
+    const clickedID = element.id;
     const updatedTodo = {
-        text: '????',
+        text: element.innerText,
         completed: true
     };
     const jsonnedTodo = JSON.stringify(updatedTodo);
     
     const xhr = new XMLHttpRequest();
-    xhr.open('PUT', 'http://localhost:3000/todos/3');
+    xhr.open('PUT', `http://localhost:3000/todos/${clickedID}`);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = handleData;
     xhr.send(jsonnedTodo);
 }
 
+function markIncomplete(element){
+    event.preventDefault();
+    const clickedID = element.id;
+    const updatedTodo = {
+        text: element.innerText,
+        completed: false
+    };
+    const jsonnedTodo = JSON.stringify(updatedTodo);
+    
+    const xhr = new XMLHttpRequest();
+    xhr.open('PUT', `http://localhost:3000/todos/${clickedID}`);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = handleData;
+    xhr.send(jsonnedTodo);
+}
+
+
 function handleData(event) {
-    console.log(event.target.responseText);
+    event.preventDefault();
+    const todos = JSON.parse(event.target.responseText);
+    const todoList = document.querySelector('.todo-list');
+
+    for (let i = 0; i < todos.length; i++) {
+        let todoItem = document.createElement('li');
+        todoItem.innerText = todos[i].text;
+        todoItem.id = todos[i].id;
+        if (todos[i].completed){
+            todoItem.style.textDecoration = 'line-through';
+        } else {
+            todoItem.style.textDecoration = '';
+        }
+        todoItem.addEventListener('click', toggleDone);
+        todoList.appendChild(todoItem);
+    }
+}
+
+
+
+function toggleDone(event){
+    const clickedElement = event.target;
+
+    if(clickedElement.style.textDecoration === 'line-through'){
+        clickedElement.style.textDecoration = '';
+        markIncomplete(clickedElement);
+    } else {
+        clickedElement.style.textDecoration = 'line-through';
+        markComplete(clickedElement);
+    }
+}
+
+function deleteCompleted(event){
+    event.preventDefault();
+
+    const todoList = Array.from(document.querySelectorAll("li"));
+    
+    for (let i = 0; i < todoList.length; i++){
+        if (todoList[i].style.textDecoration === 'line-through'){
+            let deleteIndex = todoList[i].id;
+            const xhr = new XMLHttpRequest();
+            xhr.open('DELETE', `http://localhost:3000/todos/${deleteIndex}`);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.onload = handleData;
+            xhr.send();
+        }
+    }
+
+    
 }
